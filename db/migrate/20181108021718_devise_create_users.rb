0fb48ef31b2313886ac354c3ceb7f3306402b420
Ryan814 <rytg156@gmail.com>
2018-11-08 10:33:10 +0800

    create users table with profile info

--- db/migrate/20181108021718_devise_create_users.rb
+++ db/migrate/20181108021718_devise_create_users.rb
@@ -4,8 +4,15 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string  :email,              null: false, default: ""
+      t.string  :encrypted_password, null: false, default: ""
+      t.string  :name
+      t.string  :gender
+      t.integer :age
+      t.string  :region
+      t.string  :phone
+      t.string  :avatar
+
 
       ## Recoverable
       t.string   :reset_password_token
